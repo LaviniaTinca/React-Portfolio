501b568fe18eefcab9ec05ed1d52706d38e6bf10
Lavinia Tinca <laviniaanamariatinca@gmail.com>
2024-02-14 12:54:35 +0200

    updated dummy data

--- src/utils/data/dummyData.js
+++ src/utils/data/dummyData.js
@@ -1,19 +1,6 @@
 export const items = [
   {
     id: 1,
-    title: "NFT Market",
-    images: [
-      "/projects/NFT/capture1.png",
-      "/projects/NFT/capture2.png",
-      "/projects/NFT/capture3.png",
-    ],
-    technologies: "React js, Scss",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    demo: "",
-    path: "https://lambent-fox-77811d.netlify.app/",
-  },
-  {
-    id: 2,
     title: "Agrikon App",
     images: [
       "/projects/agricon/agricon1.png",
@@ -21,10 +8,11 @@ export const items = [
       "/projects/agricon/agricon3.png",
     ],
     technologies: "React js, Scss",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "Agrikon App is a project for a farm company and is made after a Figma design. The project is 'in progress'",
     demo: "",
-    path: "",
+    path: "https://agrikon-l.netlify.app/",
   },
+
   {
     id: 3,
     title: "Quiz Grad",
@@ -36,8 +24,8 @@ export const items = [
       "/projects/ddroidd/5.png",
       "/projects/ddroidd/6.png",
     ],
-    technologies: "React js, Tailwind & shadcn/ui",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    technologies: "React js, Typescript, Tailwind & shadcn/ui",
+    desc: "QuizGrad app with Auth0 authentication, based on two roles admin and user, admin & user section",
     demo: "projects/ddroidd/demoDdroidd.gif",
     path: "",
   },
@@ -63,5 +51,19 @@ export const items = [
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     demo: "",
     path: "",
+  },
+
+  {
+    id: 1,
+    title: "NFT Market",
+    images: [
+      "/projects/NFT/capture1.png",
+      "/projects/NFT/capture2.png",
+      "/projects/NFT/capture3.png",
+    ],
+    technologies: "React js, Scss",
+    desc: " Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    demo: "",
+    path: "https://lambent-fox-77811d.netlify.app/",
   },
 ];
